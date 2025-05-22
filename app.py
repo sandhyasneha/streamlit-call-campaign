6bb9f1237e87dbb0b7ca14b772468be2cf6fd049
ram <chan2006@gmail.com>
2025-05-22 22:32:10 +0530

    Initial clean commit - Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from twilio.rest import Client
+
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
